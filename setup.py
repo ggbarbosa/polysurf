f85b6c8935511282267cb813932f0102444088f6
Gabriel Gouveia Barbosa <153515618+ggbarbosa@users.noreply.github.com>
2025-09-27 13:05:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="polysurf",
     version="0.2.0",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "ase",
         "numpy",
